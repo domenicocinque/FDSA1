427db4ede204fc3c6ab50bd73f0b87fc9dfd72ea
domenicocinque <72546227+domenicocinque@users.noreply.github.com>
2020-10-17 18:22:17 +0200

    Create gauss_module.py

---
+++ gauss_module.py
@@ -0,0 +1,61 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from scipy.signal import convolve2d as conv2
+
+
+
+"""
+Gaussian function taking as argument the standard deviation sigma
+The filter should be defined for all integer values x in the range [-3sigma,3sigma]
+The function should return the Gaussian values Gx computed at the indexes x
+"""
+def gauss(sigma):
+    sigma = int(sigma)
+    Gx = [] 
+    x = [] 
+    for i in range(-3*sigma,3*sigma + 1):
+        Gx.append((1/math.sqrt(2*math.pi*sigma**2))*math.exp(-i**2/(2*sigma**2)))
+        x.append(i)
+    return Gx, x
+
+
+"""
+Implement a 2D Gaussian filter, leveraging the previous gauss.
+Implement the filter from scratch or leverage the convolve2D method (scipy.signal)
+Leverage the separability of Gaussian filtering
+Input: image, sigma (standard deviation)
+Output: smoothed image
+"""
+def gaussianfilter(img, sigma):
+    smooth_img = []
+    for i in range(len(img)):
+        temp = list(np.convolve(img[i], gauss(sigma)[0], mode='valid'))
+        smooth_img.append(temp)
+    smooth_img = list(np.transpose(np.array(smooth_img)))
+    smooth_img2 = []
+    for i in range(len(smooth_img)):
+        temp = list(np.convolve(smooth_img[i], gauss(sigma)[0], mode='valid'))
+        smooth_img2.append(temp)
+    smooth_img2 = list(np.transpose(np.array(smooth_img2)))
+    return smooth_img2
+
+
+"""
+Gaussian derivative function taking as argument the standard deviation sigma
+The filter should be defined for all integer values x in the range [-3sigma,3sigma]
+The function should return the Gaussian derivative values Dx computed at the indexes x
+"""
+def gaussdx(sigma):
+
+    #...
+    
+    return Dx, x
+
+
+
+def gaussderiv(img, sigma):
+
+    #...
+    
+    return imgDx, imgDy
