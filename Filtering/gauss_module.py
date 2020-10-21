563c27fa48811b8e2a6cfacb7c87720edc863987
domenicocinque <domenico.cinque98@gmail.com>
2020-10-21 19:01:04 +0200

    Minor adjustments
    Reformatting

--- Filtering/gauss_module.py
+++ Filtering/gauss_module.py
@@ -3,8 +3,6 @@ import math
 import matplotlib.pyplot as plt
 from scipy.signal import convolve2d as conv2
 
-
-
 """
 Gaussian function taking as argument the standard deviation sigma
 The filter should be defined for all integer values x in the range [-3sigma,3sigma]
@@ -13,12 +11,12 @@ The function should return the Gaussian values Gx computed at the indexes x
 
 
 def gauss(sigma):
-    sigma = int(sigma)
-    Gx = [] 
-    x = [] 
-    for i in range(-3*sigma,3*sigma + 1):
-        Gx.append((1/math.sqrt(2*math.pi*sigma**2))*math.exp(-i**2/(2*sigma**2)))
-        x.append(i)
+    sigma = int(sigma) #TODO: Is this useful?
+    Gx = np.array([])
+    x = np.array([])
+    for i in range(-3 * sigma, 3 * sigma + 1):
+        Gx = np.append(Gx, (1 / math.sqrt(2 * math.pi * sigma ** 2)) * math.exp(-i ** 2 / (2 * sigma ** 2)))
+        x = np.append(x , i)
     return Gx, x
 
 
@@ -32,10 +30,10 @@ Output: smoothed image
 
 
 def gaussianfilter(img, sigma):
-    smooth_img =[]
+    smooth_img = []
     for i in range(len(img)):
         smooth_img.append(np.convolve(img[i], gauss(sigma)[0], mode='valid'))
-    smooth_img = np.array(np.transpose(np.array(smooth_img)))
+    smooth_img = np.transpose(np.array(smooth_img))
     smooth_img2 = []
     for i in range(len(smooth_img)):
         smooth_img2.append(np.convolve(smooth_img[i], gauss(sigma)[0], mode='valid'))
@@ -47,16 +45,15 @@ Gaussian derivative function taking as argument the standard deviation sigma
 The filter should be defined for all integer values x in the range [-3sigma,3sigma]
 The function should return the Gaussian derivative values Dx computed at the indexes x
 """
-def gaussdx(sigma):
 
-    #...
-    
+
+def gaussdx(sigma):
+    # ...
+
     return Dx, x
 
 
-
 def gaussderiv(img, sigma):
+    # ...
 
-    #...
-    
     return imgDx, imgDy
